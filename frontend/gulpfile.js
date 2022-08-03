f80c025f0a364508690ef7ff3da70c19c2875793
глеб зайцев <z.gl3b@yandex.ru>
2022-08-03 14:32:59 +0300

    added frontend dir

---
+++ frontend/gulpfile.js
@@ -0,0 +1,27 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'))
+const browser = require('browser-sync').create()
+
+function style() {
+    return gulp.src('./src/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./public'))
+        .pipe(browser.stream())
+}
+
+function watch() {
+    browser.init({
+        server : {
+            baseDir: './public'
+        }
+    })
+
+    gulp.watch('./src/*.scss', style)
+    gulp.watch('./public/*.html').on('change', browser.reload)
+    gulp.watch('./public/**/*.js').on('change', browser.reload)
+}
+
+exports.watch = watch;
+exports.style = style;
+exports.default = watch;
+
