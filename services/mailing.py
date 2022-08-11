9edcec05c8ae6bac45535c4eb7f73755a83bfe2b
глеб зайцев <z.gl3b@yandex.ru>
2022-08-11 18:12:16 +0300

    [mailing] send confirmation email

--- services/mailing.py
+++ services/mailing.py
@@ -2,5 +2,5 @@ from django.core.mail import EmailMessage
 
 
 def send_mail(subject: str, body: str, to: list) -> int:
-    message = EmailMessage(subject, body, None, to=to)
+    message = EmailMessage(subject, body, 'noreply@gmail.com', to=to)
     return message.send()
