5d10b62a8c24c1929c820b55f49f68d90cd14b76
глеб зайцев <z.gl3b@yandex.ru>
2022-08-14 18:34:37 +0300

    [mailing] small fix

--- blog/tasks.py
+++ blog/tasks.py
@@ -11,10 +11,12 @@ User = get_user_model()
 def send_notification_mail_about_new_article():
     """Celery task. Присылает письмо о новой статье всем подписавшися пользователям. Not implemented!"""
     users_email = [user.email for user in User.objects.filter(subscribed=True)]
-    not_notified_articles = list(BlogItem.objects.filter(notified=False))
+    new_articles = list(BlogItem.objects.filter(notified=False))
+    if not new_articles:
+        return
     send_html_template(subject="New articles", template_path=Path("mailing/new_article.html"),
-                       context={"articles": not_notified_articles}, to=users_email)
+                       context={"articles": new_articles}, to=users_email)
 
-    for article in not_notified_articles:
+    for article in new_articles:
         article.notified = True
         article.save()
