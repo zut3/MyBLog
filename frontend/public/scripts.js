491625430c71d426a29424263ae6d3003023d271
глеб зайцев <z.gl3b@yandex.ru>
2022-12-27 16:28:22 +0300

    fixed frontend

--- frontend/public/scripts.js
+++ frontend/public/scripts.js
@@ -1,1 +1,11 @@
-console.log("Hello world!");
+console.log('hello')
+const forms = document.querySelectorAll('form');
+
+forms.forEach(
+    el => el.addEventListener('submit', onSubmit)
+)
+
+function onSubmit(e) {
+    e.preventDefault();
+}
+
